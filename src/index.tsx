95a38a4ecd5a896cdf0ad0c622548f58d6bb28d5
jisaac89 <josephisaac89@gmail.com>
2018-01-30 23:09:17 -0800

    mobx provider added to entry

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,27 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import 'core-js';
+
+import { Provider } from 'mobx-react';
+
+import {appStore, authStore, prescribeStore, prescriptionsStore,patientsStore} from './stores/_GlobalStore'
 
 import Entry from './components/Entry';
 
-import 'core-js';
+const stores = {
+ appStore,
+ authStore,
+ prescribeStore,
+ prescriptionsStore,
+ patientsStore
+};
+
+// debug
+window['stores'] = stores;
 
 ReactDOM.render(
-    <Entry />,
+    <Provider {...stores}>
+        <Entry />
+    </Provider>,
     document.getElementById("root")
 ); 
